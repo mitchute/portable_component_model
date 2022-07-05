0227b80eb77059510d9eff493122d1c58e91c3b4
Edwin Lee <leeed2001@gmail.com>
2022-07-05 15:02:32 -0500

    Small tweak to GHE unit test to add TODO

--- test/GHE.unit.cpp
+++ test/GHE.unit.cpp
@@ -1,6 +1,7 @@
 #include "doctest.h"
 #include <GHE.h>
 #include <sstream>
+#include <iostream>
 
 TEST_CASE("Test the GHE Model") {
     // Setup output streams
@@ -9,11 +10,13 @@ TEST_CASE("Test the GHE Model") {
 
     // Create the GHE model
     int num_time_steps = 8760;  //num of iterations
-    GHE ghe(&output_string, &debug_string, num_time_steps);
+    GHE ghe(&output_string, &debug_string, num_time_steps);  // TODO: Consider pulling time step loop out to here
     output_string << "n" << "," << "GHE Load" << "," << "ghe_Tin (HP_Tout)" << "," << "ghe_Tout (HP_Tin)" <<  "," << "bldgload" << "\n";
     debug_string << "n" << "," << "g_data[n]" << "," << "q_lntts[n]" << "," << "q_time[n]" << "\n";
 
     // Run the model
     ghe.main_model(num_time_steps);
     CHECK(ghe.mdot == 0.2);
+
+    //CHECK(ghe.temp)
 }
